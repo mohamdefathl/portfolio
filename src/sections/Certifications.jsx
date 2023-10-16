7dc271a3a232cb8736f38a0ce0e61a8b203bef04
muhamedfathl <86556245+mohamdefathl@users.noreply.github.com>
2023-10-16 20:54:58 +0300

    das

--- src/sections/Certifications.jsx
+++ src/sections/Certifications.jsx
@@ -8,36 +8,9 @@ import first from '../assets/magp.png'
 import second from '../assets/logo.png'
 import meta from '../assets/meta.png'
 import yemen from '../assets/yemen.png'
+import fast from '../assets/fast.jpeg'
 export default function Certifications() {
-    const tags = [
-        {
-            tagx: [
-                {
-                    name: "react",
-                    color: "blue-text-gradient",
-                },
-                {
-                    name: "django",
-                    color: "green-text-gradient",
-                },
-                {
-                    name: "mysql",
-                    color: "pink-text-gradient",
-                }
-            ],
-            tagy: [
-                {
-                    name: "react",
-                    color: "blue-text-gradient",
-                },
-                {
-                    name: "tailwind",
-                    color: "green-text-gradient",
-                },
-            ],
-        }
 
-    ]
     const list = [{
         name: 'MAGP',
         description: 'The MAGP project is a project that manages and archives graduation projects for students at the university level in the Republic of Yemen. ',
@@ -56,7 +29,8 @@ export default function Certifications() {
                 color: "pink-text-gradient",
             }
         ],
-    }, {
+    },
+    {
         name: 'My Portfolio',
         description: 'My portfolio website, where i can show my learning , training , skills , works and the contact information.',
         img: second,
@@ -70,7 +44,23 @@ export default function Certifications() {
                 color: "green-text-gradient",
             },
         ]
-    }, {
+    },
+    {
+        name: 'Fast Shopping',
+        description: 'A mobile app and website that creates an application and system connecting wholesalers with retailers or any commercial entity with their customers.',
+        img: fast,
+        tag: [
+            {
+                name: "django",
+                color: "green-text-gradient",
+            },
+            {
+                name: "flutter",
+                color: "blue-text-gradient",
+            },
+        ]
+    },
+    {
         name: 'Meta Front-End Developer Professional Certificate',
         description: 'While obtaining this certificate, I have learned (Html , Css , Javascript , git , ReactJS , UX/UI design)',
         img: meta,
@@ -98,7 +88,7 @@ export default function Certifications() {
                 variants={fadeIn('up', 'spring', 1.4, 1.5)}
                 className='my-[10px] text-gray-400 text-sm font-medium w-[70%]'
             >
-            Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively
+                Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively
             </motion.p>
             <div className='grid grid-cols-2 gap-[50px]'>
                 {list.map((item, i) => {
